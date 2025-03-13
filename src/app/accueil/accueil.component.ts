14112a9c1cb630856bbf9bd6cbc482c5b14e4a75
JARRY Sullivan <sullivan.jarry@orange.fr>
2025-03-13 19:02:40 +0100

    correction redirection

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-accueil',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './accueil.component.html',
   styleUrl: './accueil.component.css'
 })
